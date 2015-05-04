cbcec03b706e976ecb7108040a7e7799218dec5f
ciscorucinski <ciscorucinski@gmail.com>
2015-05-05 03:02:34 +0900

    Changed the listener to give the UI the full equation (ie. 3 + 10 = 13) verses just the answer (ie. 13)

--- app/src/main/java/com/facebook/rucinskic/contrivedcalculator/BinaryOperation.java
+++ app/src/main/java/com/facebook/rucinskic/contrivedcalculator/BinaryOperation.java
@@ -2,15 +2,22 @@ package com.facebook.rucinskic.contrivedcalculator;
 
 public enum BinaryOperation {
 
-    Add         (new Operation(new Add())),
-    Subtract    (new Operation(new Subtract())),
-    Multiply    (new Operation(new Multiply())),
-    Divide      (new Operation(new Divide()));
+    Add         ("+", new Operation(new Add())),
+    Subtract    ("-", new Operation(new Subtract())),
+    Multiply    ("×", new Operation(new Multiply())),
+    Divide      ("÷", new Operation(new Divide()));
 
     private final Operation operation;
+    private final String symbol;
 
-    BinaryOperation(Operation operation) { this.operation = operation; }
+    BinaryOperation(String symbol, Operation operation) {
+
+        this.operation = operation;
+        this.symbol = symbol;
+
+    }
 
     public Operation getOperation() { return this.operation; }
+    public String getSymbol() { return this.symbol; }
 
 }
--- app/src/main/java/com/facebook/rucinskic/contrivedcalculator/OperationClick.java
+++ app/src/main/java/com/facebook/rucinskic/contrivedcalculator/OperationClick.java
@@ -4,21 +4,19 @@ import android.view.View;
 
 public class OperationClick {
 
-    Operation operation;
+    BinaryOperation operator;
     View.OnClickListener listener;
 
-    public OperationClick(BinaryOperation operator) {
-
-        this.operation = operator.getOperation();
-
-    }
+    public OperationClick(BinaryOperation operator) { this.operator = operator; }
 
     public View.OnClickListener listenerOn(final Calculator UI) {
 
         if (listener != null) return listener;
         return listener = new View.OnClickListener() {
 
-            double num1, num2, total;
+            double  num1, num2, total;
+            String result;
+            Operation operation = operator.getOperation();
 
             @Override
             public void onClick(View v) {
@@ -27,8 +25,9 @@ public class OperationClick {
                 num2 = Double.parseDouble(UI.txtNumber2.getText().toString());
 
                 total = operation.execute(num1, num2);
+                result = String.format("%s %s %s = %s", num1, operator.getSymbol(), num2, total);
 
-                UI.txtResult.setText(Double.toString(total));
+                UI.txtResult.setText(result);
 
             }
 
